463a56e07375d7db2efb588efc2042f8e1b3be0a
guanxi.chen <553882594@qq.com>
2016-09-19 18:16:50 +0800

    修改一下

--- app/src/main/java/com/xianzhifengshui/activity/MainActivity.java
+++ app/src/main/java/com/xianzhifengshui/activity/MainActivity.java
@@ -2,7 +2,6 @@ package com.xianzhifengshui.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.xianzhifengshui.R;
