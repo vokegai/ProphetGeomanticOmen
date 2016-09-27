d389cf1a20dbc50c2375304bb09943bfc336872f
陈冠希 <553882594@qq.com>
2016-09-27 20:05:41 +0800

    完善工具类包

--- app/src/main/java/com/xianzhifengshui/utils/EncryptUtils.java
+++ app/src/main/java/com/xianzhifengshui/utils/EncryptUtils.java
@@ -246,6 +246,20 @@ public class EncryptUtils {
         return bytes2HexString(encryptSHA256(data));
     }
 
+    private static String bytes2HexString(byte[] bytes) {
+        String s = "";
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(bytes[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+
+            s = s+hex;
+        }
+
+        return s;
+    }
+
     /**
      * SHA256加密
      *
@@ -496,6 +510,29 @@ public class EncryptUtils {
     public static byte[] decryptHexString3DES(String data, byte[] key) {
         return decrypt3DES(hexString2Bytes(data), key);
     }
+
+    private static byte[] hexString2Bytes(String hexString) {
+
+        if (hexString == null || hexString.equals("")) {
+            return null;
+        }
+        hexString = hexString.toUpperCase();
+        int length = hexString.length() / 2;
+        char[] hexChars = hexString.toCharArray();
+        byte[] d = new byte[length];
+        for (int i = 0; i < length; i++) {
+            int pos = i * 2;
+            d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
+
+        }
+        return d;
+
+    }
+
+    private static byte charToByte(char c) {
+        return (byte) "0123456789ABCDEF".indexOf(c);
+    }
+
 
     /**
      * 3DES解密
